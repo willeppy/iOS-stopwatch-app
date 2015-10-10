5e5f410f6f12a06b08b9e5d0528ad8d21a0f7c52
Will Epperson <wepperson@wills-macbook-pro.local>
2015-10-10 00:34:31 -0400

    Updated code

--- Stopwatch/ViewController.swift
+++ Stopwatch/ViewController.swift
@@ -8,18 +8,60 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var numLabel: UILabel!
+    var timer = NSTimer()
+    var timeCount = 0;
+    var hasBegun:Bool = false;
+    
+    func increment() {
+        timeCount++
+        numLabel.text = "\(timeCount) s"
+    }
+    
+    // reset timer
+    @IBAction func reset(sender: AnyObject) {
+        timer.invalidate() // essentially breaks timer / stops it
+        timeCount = 0
+        hasBegun = false
+        numLabel.text = "0 sec"
+    }
+    
+    // play timer
+    @IBAction func play(sender: AnyObject) {
+        
+        if !hasBegun {
+            
+            // timer is set equal to NSTimer object (NS from Jobs's NextStep company)
+            // @param 1 : wait time, 1 means wait 1 sec, 2 means wait 2 secs between calls
+            // @param 2 : target object (?)
+            // @param 3 : selector is method to be called every 1 second
+            // @param 4 : userInfo is not used here
+            // @param 5 : repeats means repeat action
+            timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: Selector("increment"), userInfo: nil, repeats: true)
+            
+            hasBegun = true
+        }
+    }
+    
+    // pause timer
+    @IBAction func pause(sender: AnyObject) {
+        timer.invalidate() // essentially breaks timer / stops it
+        hasBegun = false
+    }
+    
+    
+    // X-Code auto generated methods
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
 }
-
